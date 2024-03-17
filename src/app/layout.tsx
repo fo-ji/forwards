978291ed6f3ff85773b0f79b7a97df1b6aaad1a1
fo-ji <fumiya1753@gmail.com>
2024-03-17 14:26:01 +0900

    react-axeのセットアップ

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,9 @@
 import type { Metadata } from 'next';
 
 import { Inter } from 'next/font/google';
+
 import './globals.css';
+import { AppProvider } from '@/providers/app';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -17,7 +19,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <AppProvider>{children}</AppProvider>
+      </body>
     </html>
   );
 }
---
+++ src/providers/app.tsx
@@ -0,0 +1,19 @@
+'use client';
+
+import React, { type ReactNode } from 'react';
+
+import ReactDOM from 'react-dom';
+
+import { IS_BROWSER, IS_PRODUCTION } from '@/config/constants';
+
+if (IS_BROWSER && !IS_PRODUCTION) {
+  const axe = require('@axe-core/react');
+  axe(React, ReactDOM, 1000);
+}
+
+type AppProviderProps = {
+  children: ReactNode;
+};
+export const AppProvider = ({ children }: AppProviderProps) => {
+  return <>{children}</>;
+};
