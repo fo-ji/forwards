1efd8383be2cdffd3de0587160279ddca2e1d5f2
fo-ji <fumiya1753@gmail.com>
2024-03-29 16:52:57 +0900

    storybookのセットアップ

---
+++ src/components/ui/button.stories.tsx
@@ -0,0 +1,55 @@
+import { Button } from './button';
+
+import type { Meta, StoryObj } from '@storybook/react';
+
+const meta = {
+  title: 'components/ui/button',
+  component: Button,
+  tags: ['autodocs'],
+} satisfies Meta<typeof Button>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    variant: 'default',
+    children: 'Button',
+  },
+};
+
+export const Destructive: Story = {
+  args: {
+    variant: 'destructive',
+    children: 'Button',
+  },
+};
+
+export const Outline: Story = {
+  args: {
+    variant: 'outline',
+    children: 'Button',
+  },
+};
+
+export const Secondary: Story = {
+  args: {
+    variant: 'secondary',
+    children: 'Button',
+  },
+};
+
+export const Ghost: Story = {
+  args: {
+    variant: 'ghost',
+    children: 'Button',
+  },
+};
+
+export const Link: Story = {
+  args: {
+    variant: 'link',
+    children: 'Button',
+  },
+};
